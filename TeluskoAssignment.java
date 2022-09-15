526402ff2c27f8e4865a9905dc35b69535eb427a
wfm23383 <113623977+wfm23383@users.noreply.github.com>
2022-09-15 20:15:44 +0400

    TeluskoAssignment1

---
+++ TeluskoAssignment.java
@@ -0,0 +1,14 @@
+
+public class SecondAssignment {
+	public static void main(String agrs[])
+	{
+		for (int f=1;f<=6;f++)
+			{
+				for (int i=1;i<=f;i++)
+				{
+				System.out.print(i + " ");
+				}
+				System.out.println();
+			}
+	}
+}
